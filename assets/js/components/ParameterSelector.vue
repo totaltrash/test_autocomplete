d447943a077da686508851f1c490a7d8fcecce8d
Darren Black <d.black@federationtraining.edu.au>
2019-11-20 15:52:14 +1100

    Add highlighting

--- assets/js/components/ParameterSelector.vue
+++ assets/js/components/ParameterSelector.vue
@@ -3,13 +3,23 @@
         <div class="d-none">
             <input v-for="p in selectedItems" :key="p.id" type="checkbox" :name="fieldName" :value="p.id" checked />
         </div>
-        <input type="text" @keydown.esc="filter=''" v-model="filter" class="form-control" placeholder="Search for parameter" ref="filter" />
+        <input type="text" @keydown.esc="filter=''" v-model="filter" class="form-control" :placeholder="searchText" ref="filter" />
         <b-list-group class="mt-1 shadow-lg parameter-list-group">
-            <b-list-group-item v-if="filtered.length > 0" variant="light" class="font-italic font-weight-bold">Add parameter:</b-list-group-item>
-            <b-list-group-item href="#" @keydown.esc="$refs.filter.focus()" @click="select(p.id)" v-for="p in filtered" :key="p.id">{{ p.label }}</b-list-group-item>
+            <b-list-group-item
+                v-if="filtered.length == 0 && filter !== '' && filter.length >= minFilterLength"
+                class="font-italic"
+            >{{ emptySearchText }}</b-list-group-item>
+            <b-list-group-item
+                href="#"
+                @keydown.esc="$refs.filter.focus()"
+                @click="select(p.id)"
+                v-for="p in filtered"
+                :key="p.id"
+                v-html="highlight(p.label)"
+            ></b-list-group-item>
         </b-list-group>
         <b-list-group class="mt-2">
-            <b-list-group-item v-if="selectedItems.length == 0" class="font-italic">No parameters selected</b-list-group-item>
+            <b-list-group-item v-if="selectedItems.length == 0" class="font-italic">{{ emptyText }}</b-list-group-item>
             <b-list-group-item v-for="p in selectedItems" :key="p.id" class="d-flex justify-content-between align-items-center">
                 {{ p.label }}
                 <a @click.prevent="unselect(p.id)" href="#">
@@ -34,6 +44,26 @@ export default {
     selected: {
       type: Array,
       required: true
+    },
+    searchText: {
+      type: String,
+      required: false,
+      default: 'Search'
+    },
+    emptyText: {
+      type: String,
+      required: false,
+      default: 'None selected'
+    },
+    emptySearchText: {
+      type: String,
+      required: false,
+      default: 'None found'
+    },
+    minFilterLength: {
+      type: Number,
+      required: false,
+      default: 2
     }
   },
   data() {
@@ -52,10 +82,13 @@ export default {
     },
     filtered() {
       let filter = this.filter.toLowerCase()
-      if (filter === '' || filter.length < 2) {
+      if (filter === '' || filter.length < this.minFilterLength) {
         return []
       }
       return this.available.filter(p => p.label.toLowerCase().includes(filter))
+    },
+    escapedFilter() {
+      return this.escapeRegExp(this.sanitize(this.filter))
     }
   },
   methods: {
@@ -69,6 +102,20 @@ export default {
         selectedId => selectedId !== id
       )
       this.$refs.filter.focus()
+    },
+    highlight(text) {
+      text = this.sanitize(text)
+      if (this.filter.length === 0) {
+        return text
+      }
+      const re = new RegExp(this.escapedFilter, 'gi')
+      return text.replace(re, `<strong>$&</strong>`)
+    },
+    sanitize(text) {
+      return text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    },
+    escapeRegExp(str) {
+      return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
     }
   }
 }
